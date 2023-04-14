72b7341afd266f283532b9ccfd2960306d8747d4
Ahmad Alhariri‏ <0ahmad.haboosh0@gmail.com>
2023-04-14 20:57:49 +0300

    finish

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:deep_dive/core/utils/assets_manager.dart';
+import 'package:deep_dive/core/utils/strings_manager.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -15,6 +16,13 @@ class SplashViewBody extends StatelessWidget {
           AssetsManager.logo,
           width: MediaQuery.of(context).size.width / 2,
           height: MediaQuery.of(context).size.width / 2,
+        ),
+        const SizedBox(
+          height: 10.0,
+        ),
+        Text(
+          StringsManager.splashText,
+          textAlign: TextAlign.center,
         )
       ],
     );
