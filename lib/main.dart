694bdbf3430be1dd1575002a2e9b250b39ada2b9
Ahmad Alhariri‏ <0ahmad.haboosh0@gmail.com>
2023-04-15 05:40:59 +0300

    add font family

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:google_fonts/google_fonts.dart';
+
 import '/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -15,7 +17,8 @@ class DeepDive extends StatelessWidget {
     return  GetMaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor:  kPrimaryColor
+        scaffoldBackgroundColor:  kPrimaryColor,
+        textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme)
       ),
       home:const SplashView(),
     );
