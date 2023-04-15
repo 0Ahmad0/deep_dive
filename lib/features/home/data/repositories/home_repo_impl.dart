6b70007f0e43c39678bcd467676e75fc05693fa6
Ahmad Alhariri‏ <0ahmad.haboosh0@gmail.com>
2023-04-16 00:41:56 +0300

    created serverfailuer from dio error

---
+++ lib/features/home/data/repositories/home_repo_impl.dart
@@ -0,0 +1,52 @@
+import 'package:dartz/dartz.dart';
+import 'package:deep_dive/features/home/data/data_sources/home_locale_data_sources.dart';
+import 'package:deep_dive/features/home/data/data_sources/home_remote_data_sources.dart';
+import 'package:dio/dio.dart';
+import '/core/errors/failure.dart';
+import '/features/home/domain/entities/book_entity.dart';
+import '/features/home/domain/repositories/home_repo.dart';
+
+class HomeRepoImpl extends HomeRepo {
+  final HomeRemoteDataSource homeRemoteDataSource;
+  final HomeLocalDataSource homeLocalDataSource;
+
+  HomeRepoImpl(
+      {required this.homeRemoteDataSource, required this.homeLocalDataSource});
+
+  @override
+  Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks() async {
+    try {
+      List<BookEntity> books;
+      books = homeLocalDataSource.fetchFeaturedBooks();
+      if (books.isNotEmpty) {
+        return right(books);
+      }
+      books = await homeRemoteDataSource.fetchFeaturedBooks();
+      return right(books);
+    } on Exception catch (e) {
+      if (e is DioError) {
+        return left(ServerFailure.fromDioError(e));
+      }
+      return left(ServerFailure(e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure, List<BookEntity>>> fetchNewestBooks() async {
+    try {
+      List<BookEntity> books;
+
+      books = homeLocalDataSource.fetchNewestBooks();
+      if (books.isNotEmpty) {
+        return right(books);
+      }
+      books = await homeRemoteDataSource.fetchNewestBooks();
+      return right(books);
+    } on Exception catch (e) {
+      if (e is DioError) {
+        return left(ServerFailure.fromDioError(e));
+      }
+      return left(ServerFailure(e.toString()));
+    }
+  }
+}
